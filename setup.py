93b7341e5df49b621702acf2fae68298b6dcadc5
zhaofeng-shu33 <616545598@qq.com>
2021-11-05 09:42:33 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ with open("README.md") as fh:
 setup(
     name='ivp_enhanced',
     version='0.0.1',
-    long_description = long_description,
-    long_description_content_type="text/markdown"
+    packages=find_packages(include=['ivp_enhanced', 'ivp_enhanced.*']),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/zhaofeng-shu33/OCBO',
+    author='zhaofeng-shu33',
+    author_email='616545598@qq.com',
+    description='enhance some missing features of scipy.integrate.solve_ivp'
 )
