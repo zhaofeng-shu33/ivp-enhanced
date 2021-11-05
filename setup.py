5075055206bbe5ed11da002fa156418c8ee2e724
zhaofeng-shu33 <616545598@qq.com>
2021-11-05 09:48:43 +0800

    bar

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(include=['ivp_enhanced', 'ivp_enhanced.*']),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/zhaofeng-shu33/ivp_enhanced',
+    url='https://github.com/zhaofeng-shu33/ivp-enhanced',
     author='zhaofeng-shu33',
     author_email='616545598@qq.com',
     description='enhance some missing features of scipy.integrate.solve_ivp'
